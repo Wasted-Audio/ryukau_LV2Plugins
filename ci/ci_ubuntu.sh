e3f9e7f289057159add168057697a5ac2b469694
cu <cu@localhost.localdomain>
2020-05-17 09:50:12 +0900

    Add CI

---
+++ ci/ci_ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Build script for GitHub Actions.
+#
+
+set -e
+
+# Without update, some package will be 404.
+sudo apt-get update
+sudo apt-get install git pkg-config libjack-jackd2-dev libgl-dev liblo-dev
+
+# Temporary patch to DPF. See: https://github.com/DISTRHO/DPF/issues/216
+cp patch/NanoVG.cpp lib/DPF/dgl/src/NanoVG.cpp
+
+make
