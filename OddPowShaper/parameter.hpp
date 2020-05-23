697062bd0c3859f6d4b67b07fe8dc2851b3433e0
cu <cu@localhost.localdomain>
2020-05-23 23:05:47 +0900

    Add color config to OddPowShaper

--- OddPowShaper/parameter.hpp
+++ OddPowShaper/parameter.hpp
@@ -17,15 +17,15 @@
 
 #pragma once
 
+#include "../common/dsp/constants.hpp"
+#include "../common/dsp/scale.hpp"
+#include "../common/parameterinterface.hpp"
+#include "../common/value.hpp"
+
+#include <iostream>
 #include <memory>
 #include <string>
 #include <vector>
-
-#include <iostream>
-
-#include "../common/dsp/constants.hpp"
-#include "../common/dsp/scale.hpp"
-#include "../common/value.hpp"
 
 #ifdef TEST_BUILD
 static const uint32_t kParameterIsAutomable = 0x01;
@@ -66,7 +66,7 @@ struct Scales {
   static SomeDSP::LogScale<double> smoothness;
 };
 
-struct GlobalParameter {
+struct GlobalParameter : public ParameterInterface {
   std::vector<std::unique_ptr<ValueInterface>> value;
 
   GlobalParameter()
@@ -111,15 +111,35 @@ struct GlobalParameter {
   }
 #endif
 
+  size_t idLength() override { return value.size(); }
+
   void resetParameter()
   {
     for (auto &val : value) val->setFromNormalized(val->getDefaultNormalized());
   }
 
-  double getParameterValue(uint32_t index) const
+  double getNormalized(uint32_t index) const override
+  {
+    if (index >= value.size()) return 0.0;
+    return value[index]->getNormalized();
+  }
+
+  double getDefaultNormalized(uint32_t index) const override
+  {
+    if (index >= value.size()) return 0.0;
+    return value[index]->getDefaultNormalized();
+  }
+
+  double getFloat(uint32_t index) const override
   {
     if (index >= value.size()) return 0.0;
     return value[index]->getFloat();
+  }
+
+  double getInt(uint32_t index) const override
+  {
+    if (index >= value.size()) return 0.0;
+    return value[index]->getInt();
   }
 
   void setParameterValue(uint32_t index, float raw)
@@ -128,14 +148,14 @@ struct GlobalParameter {
     value[index]->setFromFloat(raw);
   }
 
-  double parameterChanged(uint32_t index, float raw)
+  double parameterChanged(uint32_t index, float raw) override
   {
     if (index >= value.size()) return 0.0;
     value[index]->setFromFloat(raw);
     return value[index]->getNormalized();
   }
 
-  double updateValue(uint32_t index, float normalized)
+  double updateValue(uint32_t index, float normalized) override
   {
     if (index >= value.size()) return 0.0;
     value[index]->setFromNormalized(normalized);
