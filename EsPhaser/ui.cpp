5ecee6c42bbc7d464bb0750a116af71570c0513d
cu <cu@localhost.localdomain>
2020-05-23 23:01:15 +0900

    Add color config to EsPhaser

--- EsPhaser/ui.cpp
+++ EsPhaser/ui.cpp
@@ -24,18 +24,8 @@
 #include <memory>
 #include <vector>
 
-#include "../common/ui.hpp"
+#include "../common/uibase.hpp"
 #include "parameter.hpp"
-
-#include "../common/gui/TinosBoldItalic.hpp"
-#include "../common/gui/barbox.hpp"
-#include "../common/gui/button.hpp"
-#include "../common/gui/checkbox.hpp"
-#include "../common/gui/knob.hpp"
-#include "../common/gui/label.hpp"
-#include "../common/gui/rotaryknob.hpp"
-#include "../common/gui/splash.hpp"
-#include "../common/gui/vslider.hpp"
 
 void CreditSplash::onNanoDisplay()
 {
@@ -50,14 +40,14 @@ void CreditSplash::onNanoDisplay()
   // Border.
   beginPath();
   rect(0, 0, width, height);
-  fillColor(backgroundColor);
+  fillColor(pal.background());
   fill();
-  strokeColor(isMouseEntered ? highlightColor : foregroundColor);
+  strokeColor(isMouseEntered ? pal.highlightMain() : pal.foreground());
   strokeWidth(2.0f);
   stroke();
 
   // Text.
-  fillColor(foregroundColor);
+  fillColor(pal.foreground());
   fontFaceId(fontId);
   textAlign(align);
 
@@ -96,10 +86,23 @@ constexpr float splashHeight = labelHeight;
 constexpr uint32_t defaultWidth = uint32_t(40 + 9.0f * knobX + labelY);
 constexpr uint32_t defaultHeight = uint32_t(40 + labelHeight + knobY);
 
-class EsPhaserUI : public PluginUI {
-public:
-  EsPhaserUI() : PluginUI(defaultWidth, defaultHeight)
+class EsPhaserUI : public PluginUIBase {
+protected:
+  void onNanoDisplay() override
   {
+    beginPath();
+    rect(0, 0, getWidth(), getHeight());
+    fillColor(palette.background());
+    fill();
+  }
+
+  DISTRHO_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(EsPhaserUI)
+
+public:
+  EsPhaserUI() : PluginUIBase(defaultWidth, defaultHeight)
+  {
+    param = std::make_unique<GlobalParameter>();
+
     setGeometryConstraints(defaultWidth, defaultHeight, true, true);
 
     fontId = createFontFromMemory(
@@ -115,38 +118,43 @@ public:
     const auto phaserTop = top0 - margin;
     const auto phaserLeft = left0;
 
-    addKnob(phaserLeft, phaserTop, knobWidth, colorBlue, "Mix", ID::mix);
-    addKnob(phaserLeft + knobX, phaserTop, knobWidth, colorBlue, "Freq", ID::frequency);
+    addKnob(phaserLeft, phaserTop, knobWidth, margin, uiTextSize, "Mix", ID::mix);
     addKnob(
-      phaserLeft + 2.0f * knobX, phaserTop, knobWidth, colorBlue, "Spread",
+      phaserLeft + knobX, phaserTop, knobWidth, margin, uiTextSize, "Freq",
+      ID::frequency);
+    addKnob(
+      phaserLeft + 2.0f * knobX, phaserTop, knobWidth, margin, uiTextSize, "Spread",
       ID::freqSpread);
-    addKnob(
-      phaserLeft + 3.0f * knobX, phaserTop, knobWidth, colorRed, "Feedback",
+    addKnob<Style::warning>(
+      phaserLeft + 3.0f * knobX, phaserTop, knobWidth, margin, uiTextSize, "Feedback",
       ID::feedback);
     addKnob(
-      phaserLeft + 4.0f * knobX, phaserTop, knobWidth, colorBlue, "Range", ID::range);
-    addKnob(phaserLeft + 5.0f * knobX, phaserTop, knobWidth, colorBlue, "Min", ID::min);
+      phaserLeft + 4.0f * knobX, phaserTop, knobWidth, margin, uiTextSize, "Range",
+      ID::range);
     addKnob(
-      phaserLeft + 6.0f * knobX, phaserTop, knobWidth, colorRed, "Cas. Offset",
+      phaserLeft + 5.0f * knobX, phaserTop, knobWidth, margin, uiTextSize, "Min",
+      ID::min);
+    addKnob(
+      phaserLeft + 6.0f * knobX, phaserTop, knobWidth, margin, uiTextSize, "Cas. Offset",
       ID::cascadeOffset);
     addKnob(
-      phaserLeft + 7.0f * knobX, phaserTop, knobWidth, colorBlue, "L/R Offset",
+      phaserLeft + 7.0f * knobX, phaserTop, knobWidth, margin, uiTextSize, "L/R Offset",
       ID::stereoOffset);
     addRotaryKnob(
-      phaserLeft + 8.0f * knobX, phaserTop, knobWidth + labelY, colorBlue, "Phase",
-      ID::phase);
+      phaserLeft + 8.0f * knobX, phaserTop, knobWidth + labelY, margin, uiTextSize,
+      "Phase", ID::phase);
 
     const auto phaserTop1 = phaserTop + knobY + margin;
     const auto phaserLeft1 = left0 + 2.25f * knobX - margin;
-    addLabel(phaserLeft1, phaserTop1, knobX * 1.2, "Stage");
+    addLabel(phaserLeft1, phaserTop1, knobX * 1.2, labelHeight, uiTextSize, "Stage");
     addTextKnob(
-      phaserLeft1 + knobX, phaserTop1, knobX, colorBlue, ID::stage, Scales::stage, false,
-      0, 1);
+      phaserLeft1 + knobX, phaserTop1, knobX, labelHeight, uiTextSize, ID::stage,
+      Scales::stage, false, 0, 1);
 
     const auto phaserLeft2 = phaserLeft1 + 2.25f * knobX;
-    addLabel(phaserLeft2, phaserTop1, knobX, "Smooth");
+    addLabel(phaserLeft2, phaserTop1, knobX, labelHeight, uiTextSize, "Smooth");
     addTextKnob(
-      phaserLeft2 + knobX, phaserTop1, knobX, colorBlue, ID::smoothness,
+      phaserLeft2 + knobX, phaserTop1, knobX, labelHeight, uiTextSize, ID::smoothness,
       Scales::smoothness, false, 3, 0);
 
     // Plugin name.
@@ -154,351 +162,8 @@ public:
     const auto splashLeft = left0;
     addSplashScreen(
       splashLeft, splashTop, 2.0f * knobX, splashHeight, 20.0f, 20.0f,
-      defaultWidth - 40.0f, defaultHeight - 40.0f, "EsPhaser");
+      defaultWidth - 40.0f, defaultHeight - 40.0f, pluginNameTextSize, "EsPhaser");
   }
-
-protected:
-  void parameterChanged(uint32_t index, float value) override
-  {
-    updateUI(index, param.parameterChanged(index, value));
-  }
-
-  void updateUI(uint32_t id, float normalized)
-  {
-    for (auto &vWidget : valueWidget) {
-      if (vWidget->id != id) continue;
-      vWidget->setValue(normalized);
-      repaint();
-      return;
-    }
-
-    for (auto &aWidget : arrayWidget) {
-      auto &idVec = aWidget->id;
-      auto iter = std::find(idVec.begin(), idVec.end(), id);
-      if (iter != idVec.end()) {
-        aWidget->setValueAt(std::distance(idVec.begin(), iter), normalized);
-        repaint();
-        return;
-      }
-    }
-  }
-
-  void updateValue(uint32_t id, float normalized) override
-  {
-    setParameterValue(id, param.updateValue(id, normalized));
-    repaint();
-    // dumpParameter(); // Used to make preset. There may be better way to do this.
-  }
-
-  void updateState(std::string /* key */, std::string /* value */) {}
-
-  void programLoaded(uint32_t index) override
-  {
-    param.loadProgram(index);
-
-    for (auto &vWidget : valueWidget)
-      vWidget->setValue(param.value[vWidget->id]->getNormalized());
-
-    for (auto &aWidget : arrayWidget) {
-      for (size_t idx = 0; idx < aWidget->id.size(); ++idx) {
-        aWidget->setValueAt(idx, param.value[aWidget->id[idx]]->getNormalized());
-      }
-    }
-
-    repaint();
-  }
-
-  void onNanoDisplay() override
-  {
-    beginPath();
-    rect(0, 0, getWidth(), getHeight());
-    fillColor(colorBack);
-    fill();
-  }
-
-private:
-  GlobalParameter param;
-
-  Color colorBack{255, 255, 255};
-  Color colorFore{0, 0, 0};
-  Color colorInactive{237, 237, 237};
-  Color colorBlue{11, 164, 241};
-  Color colorGreen{19, 193, 54};
-  Color colorOrange{252, 192, 79};
-  Color colorRed{252, 128, 128};
-
-  FontId fontId = -1;
-
-  std::vector<std::shared_ptr<Widget>> widget;
-  std::vector<std::shared_ptr<ValueWidget>> valueWidget;
-  std::vector<std::shared_ptr<ArrayWidget>> arrayWidget;
-
-  void dumpParameter()
-  {
-    std::cout << "{\n";
-    for (const auto &value : param.value)
-      std::cout << "\"" << value->getName()
-                << "\": " << std::to_string(value->getNormalized()) << ",\n";
-    std::cout << "}" << std::endl;
-  }
-
-  void addButton(float left, float top, float width, const char *title, uint32_t id)
-  {
-    auto button = std::make_shared<ToggleButton>(this, this, title, fontId);
-    button->id = id;
-    button->setSize(width, labelHeight);
-    button->setAbsolutePos(left, top);
-    button->setForegroundColor(colorFore);
-    button->setHighlightColor(colorOrange);
-    button->setTextSize(midTextSize);
-    valueWidget.push_back(button);
-  }
-
-  void addCheckbox(float left, float top, float width, const char *title, uint32_t id)
-  {
-    auto checkbox = std::make_shared<CheckBox>(this, this, title, fontId);
-    checkbox->id = id;
-    checkbox->setSize(width, labelHeight);
-    checkbox->setAbsolutePos(left, top);
-    checkbox->setForegroundColor(colorFore);
-    checkbox->setHighlightColor(colorBlue);
-    checkbox->setTextSize(uiTextSize);
-    valueWidget.push_back(checkbox);
-  }
-
-  void addLabel(int left, int top, float width, const char *name)
-  {
-    auto label = std::make_shared<Label>(this, name, fontId);
-    label->setSize(width, labelHeight);
-    label->setAbsolutePos(left, top);
-    label->setForegroundColor(colorFore);
-    label->drawBorder = false;
-    label->setTextSize(uiTextSize);
-    widget.push_back(label);
-  };
-
-  void addGroupLabel(int left, int top, float width, const char *name)
-  {
-    auto label = std::make_shared<Label>(this, name, fontId);
-    label->setSize(width, labelHeight);
-    label->setAbsolutePos(left, top);
-    label->setForegroundColor(colorFore);
-    label->drawBorder = true;
-    label->setBorderWidth(2.0f);
-    label->setTextSize(midTextSize);
-    widget.push_back(label);
-  };
-
-  void addGroupVerticalLabel(int left, int top, float width, const char *name)
-  {
-    auto label = std::make_shared<VLabel>(this, name, fontId);
-    label->setSize(width, labelHeight);
-    label->setAbsolutePos(left, top);
-    label->setForegroundColor(colorFore);
-    label->drawBorder = false;
-    label->setBorderWidth(2.0f);
-    label->setTextSize(midTextSize);
-    widget.push_back(label);
-  };
-
-  enum class LabelPosition {
-    top,
-    left,
-    bottom,
-    right,
-  };
-
-  void addKnob(
-    float left,
-    float top,
-    float width,
-    Color highlightColor,
-    const char *name,
-    uint32_t id,
-    LabelPosition labelPosition = LabelPosition::bottom)
-  {
-    auto height = width - 2.0f * margin;
-
-    auto knob = std::make_shared<Knob>(this, this);
-    knob->id = id;
-    knob->setSize(width - 2.0f * margin, height);
-    knob->setAbsolutePos(left + margin, top + margin);
-    knob->setHighlightColor(highlightColor);
-    if (id < param.value.size()) {
-      auto defaultValue = param.value[id]->getDefaultNormalized();
-      knob->setDefaultValue(defaultValue);
-      knob->setValue(defaultValue);
-    }
-    valueWidget.push_back(knob);
-
-    if (name != nullptr) addKnobLabel(left, top, width, height, name, labelPosition);
-  }
-
-  template<typename Scale>
-  void addNumberKnob(
-    float left,
-    float top,
-    float width,
-    Color highlightColor,
-    const char *name,
-    uint32_t id,
-    Scale &scale,
-    LabelPosition labelPosition = LabelPosition::bottom)
-  {
-    auto height = width - 2.0f * margin;
-
-    auto knob = std::make_shared<NumberKnob<Scale>>(this, this, fontId, scale);
-    knob->id = id;
-    knob->setSize(width - 2.0f * margin, height);
-    knob->setAbsolutePos(left + margin, top + margin);
-    knob->setHighlightColor(highlightColor);
-    auto defaultValue = param.value[id]->getDefaultNormalized();
-    knob->setDefaultValue(defaultValue);
-    knob->setValue(defaultValue);
-    valueWidget.push_back(knob);
-
-    addKnobLabel(left, top, width, height, name, labelPosition);
-  }
-
-  void addRotaryKnob(
-    float left,
-    float top,
-    float width,
-    Color highlightColor,
-    const char *name,
-    uint32_t id,
-    LabelPosition labelPosition = LabelPosition::bottom)
-  {
-    auto height = width - 2.0f * margin;
-
-    auto knob = std::make_shared<RotaryKnob>(this, this);
-    knob->id = id;
-    knob->setSize(width - 2.0f * margin, height);
-    knob->setAbsolutePos(left + margin, top + margin);
-    knob->setHighlightColor(highlightColor);
-    auto defaultValue = param.value[id]->getDefaultNormalized();
-    knob->setDefaultValue(defaultValue);
-    knob->setValue(defaultValue);
-    valueWidget.push_back(knob);
-
-    addKnobLabel(left, top, width, height, name, labelPosition);
-  }
-
-  void addKnobLabel(
-    float left,
-    float top,
-    float width,
-    float height,
-    const char *name,
-    LabelPosition labelPosition)
-  {
-    switch (labelPosition) {
-      default:
-      case LabelPosition::bottom:
-        top = top + height;
-        height = 30.0f;
-        break;
-
-      case LabelPosition::right:
-        height = width;
-        left = left + width + 10.0;
-        width *= 2.0f;
-        break;
-    }
-
-    auto label = std::make_shared<Label>(this, name, fontId);
-    label->setSize(width, height);
-    label->setAbsolutePos(left, top);
-    label->setForegroundColor(colorFore);
-    label->setTextSize(uiTextSize);
-    if (labelPosition == LabelPosition::right)
-      label->setTextAlign(ALIGN_LEFT | ALIGN_MIDDLE);
-    widget.push_back(label);
-  }
-
-  template<typename Scale>
-  void addTextKnob(
-    float left,
-    float top,
-    float width,
-    Color highlightColor,
-    uint32_t id,
-    Scale &scale,
-    bool isDecibel = false,
-    uint32_t precision = 0,
-    int32_t offset = 0)
-  {
-    auto knob = std::make_shared<TextKnob<Scale>>(this, this, fontId, scale, isDecibel);
-    knob->id = id;
-    knob->setSize(width, labelHeight);
-    knob->setAbsolutePos(left, top);
-    knob->setForegroundColor(colorFore);
-    knob->setHighlightColor(highlightColor);
-    auto defaultValue = param.value[id]->getDefaultNormalized();
-    knob->setDefaultValue(defaultValue);
-    knob->setValue(defaultValue);
-    knob->setPrecision(precision);
-    knob->offset = offset;
-    knob->setTextSize(uiTextSize);
-    valueWidget.push_back(knob);
-  }
-
-  void addSplashScreen(
-    float buttonLeft,
-    float buttonTop,
-    float buttonWidth,
-    float buttonHeight,
-    float splashLeft,
-    float splashTop,
-    float splashWidth,
-    float splashHeight,
-    const char *name)
-  {
-    auto button = std::make_shared<SplashButton>(this, name, fontId);
-    button->setSize(buttonWidth, buttonHeight);
-    button->setAbsolutePos(buttonLeft, buttonTop);
-    button->setForegroundColor(colorFore);
-    button->setHighlightColor(colorOrange);
-    button->setTextSize(pluginNameTextSize);
-    widget.push_back(button);
-
-    auto credit = std::make_shared<CreditSplash>(this, name, fontId);
-    credit->setSize(splashWidth, splashHeight);
-    credit->setAbsolutePos(splashLeft, splashTop);
-    button->setSplashWidget(credit);
-    widget.push_back(credit);
-  }
-
-  void addVSlider(float left, float top, Color valueColor, const char *name, uint32_t id)
-  {
-    // width, height = 100, 270.
-
-    const auto width = 70.0f;
-    const auto sliderHeight = 230.0f;
-
-    auto slider = std::make_shared<VSlider>(this, this);
-    slider->id = id;
-    slider->setSize(width, sliderHeight);
-    slider->setAbsolutePos(left, top);
-    auto defaultValue = param.value[id]->getDefaultNormalized();
-    slider->setDefaultValue(defaultValue);
-    slider->setValue(defaultValue);
-    slider->setHighlightColor(valueColor);
-    slider->setValueColor(valueColor);
-    slider->setBorderColor(colorFore);
-    valueWidget.push_back(slider);
-
-    top += sliderHeight + 10.0;
-
-    auto label = std::make_shared<Label>(this, name, fontId);
-    label->setSize(width, labelHeight);
-    label->setAbsolutePos(left, top);
-    label->setForegroundColor(colorFore);
-    label->setTextSize(uiTextSize);
-    widget.push_back(label);
-  }
-
-  DISTRHO_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(EsPhaserUI)
 };
 
 UI *createUI() { return new EsPhaserUI(); }
