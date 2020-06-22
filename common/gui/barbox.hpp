ee1ff34b3717137a73715b34a0919ef9d3f27715
cu <cu@localhost.localdomain>
2020-06-22 09:04:04 +0900

    Split scrollbar.hpp from barbox.hpp

--- common/gui/barbox.hpp
+++ common/gui/barbox.hpp
@@ -26,195 +26,35 @@
 #include <sstream>
 #include <string>
 
-template<typename Scrollable> class ScrollBar : public NanoWidget {
-public:
-  explicit ScrollBar(
-    NanoWidget *group, std::shared_ptr<Scrollable> parent, Palette &palette)
-    : NanoWidget(group), parent(parent), pal(palette)
-  {
-  }
-
-  void onNanoDisplay() override
-  {
-    resetTransform();
-    translate(getAbsoluteX(), getAbsoluteY());
-
-    const auto width = getWidth();
-    const auto height = getHeight();
-
-    strokeWidth(2.0f);
-    strokeColor(pal.border());
-
-    // Bar.
-    auto rightHandleL = rightPos * width - handleWidth;
-    auto barL = leftPos * width + handleWidth;
-    beginPath();
-    rect(barL, 0, rightHandleL - barL, height);
-    fillColor(pointed == Part::bar ? pal.highlightButton() : pal.highlightMain());
-    fill();
-    stroke();
-
-    // Left handle.
-    beginPath();
-    rect(leftPos * width, 0, handleWidth, height);
-    fillColor(pointed == Part::leftHandle ? pal.highlightButton() : pal.unfocused());
-    fill();
-    stroke();
-
-    // Right handle.
-    beginPath();
-    rect(rightHandleL, 0, handleWidth, height);
-    fillColor(pointed == Part::rightHandle ? pal.highlightButton() : pal.unfocused());
-    fill();
-    stroke();
-  }
-
-  bool onMouse(const MouseEvent &ev) override
-  {
-    if (contains(ev.pos) && ev.press) {
-      if (ev.button == 1) { // Left down. Grab scroll bar.
-        grabbed = pointed = hitTest(ev.pos);
-
-        if (grabbed == Part::leftHandle)
-          grabOffset = int(leftPos * getWidth()) - ev.pos.getX();
-        else if (grabbed == Part::rightHandle)
-          grabOffset = int(rightPos * getWidth()) - ev.pos.getX();
-        else if (grabbed == Part::bar)
-          grabOffset = int(leftPos * getWidth()) - ev.pos.getX();
-      } else if (ev.button == 3) { // Right down. Reset zoom.
-        leftPos = 0;
-        rightPos = 1;
-        parent->setViewRange(leftPos, rightPos);
-        repaint();
-      }
-
-      return true;
-    }
-
-    grabbed = Part::background;
-    return false;
-  }
-
-  bool onMotion(const MotionEvent &ev) override
-  {
-    auto posX
-      = std::clamp<int>(ev.pos.getX() + grabOffset, 0, getWidth()) / float(getWidth());
-    switch (grabbed) {
-      case Part::bar: {
-        auto barWidth = rightPos - leftPos;
-        leftPos = posX;
-        rightPos = leftPos + barWidth;
-        if (leftPos < 0.0f) {
-          rightPos = barWidth;
-          leftPos = 0.0f;
-        } else if (rightPos > 1.0f) {
-          leftPos = 1.0f - barWidth;
-          rightPos = 1.0f;
-        }
-
-        // Just in case.
-        leftPos = std::clamp(leftPos, 0.0f, 1.0f);
-        rightPos = std::clamp(rightPos, 0.0f, 1.0f);
-      } break;
-
-      case Part::leftHandle: {
-        setLeftPos(posX);
-      } break;
-
-      case Part::rightHandle: {
-        setRightPos(posX);
-      } break;
-
-      default:
-        pointed = hitTest(ev.pos);
-        repaint();
-        return false;
-    }
-
-    parent->setViewRange(leftPos, rightPos);
-    repaint();
-
-    return true;
-  }
-
-  bool onScroll(const ScrollEvent &ev) override
-  {
-    if (!contains(ev.pos)) return false;
-
-    const auto mouseX = float(ev.pos.getX()) / getWidth();
-    const auto delta = ev.delta.getY();
-
-    float amountL, amountR;
-    if (delta > 0) {
-      amountL = 0.5f * zoomSensi;
-      amountR = 0.5f * zoomSensi;
-    } else {
-      const auto bias = (mouseX - leftPos) / (rightPos - leftPos);
-      amountL = zoomSensi * std::clamp(bias, 0.0f, 1.0f);
-      amountR = zoomSensi * std::clamp((1.0f - bias), 0.0f, 1.0f);
-    }
-    setLeftPos(leftPos - amountL * delta);
-    setRightPos(rightPos + amountR * delta);
-
-    parent->setViewRange(leftPos, rightPos);
-    repaint();
-
-    return true;
-  }
-
-  void setHandleWidth(float width) { handleWidth = std::max(width, 0.0f); }
-
-protected:
-  enum class Part : uint8_t { background = 0, bar, leftHandle, rightHandle };
-
-  inline void setLeftPos(float x)
-  {
-    auto rightMost = std::max(rightPos - 3 * handleWidth / getWidth(), 0.0f);
-    leftPos = std::clamp(x, 0.0f, rightMost);
-  }
-
-  inline void setRightPos(float x)
-  {
-    auto leftMost = std::min(leftPos + 3 * handleWidth / getWidth(), 1.0f);
-    rightPos = std::clamp(x, leftMost, 1.0f);
-  }
-
-  Part hitTest(Point<int> point)
-  {
-    if (point.getY() < 0 || point.getY() > int(getHeight())) return Part::background;
-
-    auto left = leftPos * getWidth();
-    auto right = rightPos * getWidth();
-    auto width = handleWidth;
-    auto px = point.getX();
-
-    auto leftHandleR = left + width;
-    if (px >= left && px <= leftHandleR) return Part::leftHandle;
-
-    auto rightHandleL = right - width;
-    if (px >= rightHandleL && px <= right) return Part::rightHandle;
-
-    if (px > leftHandleR && px < rightHandleL) return Part::bar;
-
-    return Part::background;
-  }
-
-  float zoomSensi = 0.05f;
-  float handleWidth = 10;
-  float leftPos = 0;
-  float rightPos = 1;
-  int grabOffset = 0;
-
-  Part pointed = Part::background;
-  Part grabbed = Part::background;
-
-  std::shared_ptr<Scrollable> parent;
-  Palette &pal;
-};
-
 template<typename Scale> class BarBox : public ArrayWidget {
 private:
   enum class BarState : uint8_t { active, lock };
+
+  std::vector<double> defaultValue;
+  std::vector<std::vector<double>> undoValue;
+  std::vector<BarState> barState;
+  std::vector<double> active, locked; // Used to store temporary values.
+  float sliderWidth = 0;
+  float sliderMargin = 2.0f;
+  float borderWidth = 2.0f;
+  float highlightBorderWidth = 4.0f;
+  float scrollBarheight = 8.0f;
+
+  float textSize = 9.0f;
+  FontId fontId = -1;
+  Palette &pal;
+
+  Scale &scale;
+
+  Point<int> mousePosition{-1, -1};
+  Point<int> anchor{0, 0};
+  BarState anchorState = BarState::active;
+  int indexL = 0;
+  int indexR = 0;
+  int indexRange = 0;
+  bool isMouseLeftDown = false;
+  bool isMouseRightDown = false;
+  bool isMouseEntered = false;
 
 public:
   float sliderZero = 0.0f;
@@ -241,6 +81,8 @@ public:
     for (size_t i = 0; i < 4; ++i) undoValue.emplace_back(defaultValue);
 
     barState.resize(defaultValue.size(), BarState::active);
+    active.reserve(value.size());
+    locked.reserve(value.size());
   }
 
   void onNanoDisplay() override
@@ -338,9 +180,8 @@ public:
   }
 
 #define APPLY_ALGORITHM(START, FUNC)                                                     \
-  std::vector<double> active, locked;                                                    \
-  active.reserve(value.size());                                                          \
-  locked.reserve(value.size());                                                          \
+  active.resize(0);                                                                      \
+  locked.resize(0);                                                                      \
                                                                                          \
   for (size_t i = START; i < value.size(); ++i) {                                        \
     if (barState[i] == BarState::active)                                                 \
@@ -928,29 +769,4 @@ private:
     if (liveUpdateLineEdit) updateValue();
     repaint();
   }
-
-  std::vector<double> defaultValue;
-  std::vector<std::vector<double>> undoValue;
-  std::vector<BarState> barState;
-  float sliderWidth = 0;
-  float sliderMargin = 2.0f;
-  float borderWidth = 2.0f;
-  float highlightBorderWidth = 4.0f;
-  float scrollBarheight = 8.0f;
-
-  float textSize = 9.0f;
-  FontId fontId = -1;
-  Palette &pal;
-
-  Scale &scale;
-
-  Point<int> mousePosition{-1, -1};
-  Point<int> anchor{0, 0};
-  BarState anchorState = BarState::active;
-  int indexL = 0;
-  int indexR = 0;
-  int indexRange = 0;
-  bool isMouseLeftDown = false;
-  bool isMouseRightDown = false;
-  bool isMouseEntered = false;
 };
